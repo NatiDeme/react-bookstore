357eb767aba926e5a5953aa4de511e82452cca25
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-07-08 15:20:07 +0300

    Add update to README

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,14 +6,16 @@ function Book(props) {
   const mee = me.books;
   return (
     <>
-      {mee.map((i) => (
-        <div key={i} className="name-wrap">
-          <p className="book-title">{i.genre}</p>
-          <p className="book-title">{i.title}</p>
-          <p className="book-author">{i.author}</p>
-          <button type="button">Remove</button>
-        </div>
-      ))}
+      <div className="book-card">
+        {mee.map((i) => (
+          <div key={i} className="name-wrap">
+            <p className="book-title">{i.genre}</p>
+            <p className="book-title">{i.title}</p>
+            <p className="book-author">{i.author}</p>
+            <button type="button">Remove</button>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
