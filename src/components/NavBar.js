cd29cb6084ed85a9bed91c1c36412b8691242625
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-07-16 00:15:25 +0300

    Add fix to checkers

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,7 @@ function NavBar() {
         <li><Link to="/categories" className="lists cate">CATEGORIES</Link></li>
       </ul>
       <div className="image-wrap">
-        <svg xmlns="http://www.w3.org/2000/svg" className="image h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" className="image" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
         </svg>
       </div>
